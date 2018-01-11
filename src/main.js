dec3b12dd7cc4df206227da570f19e323c973f13
KR <1918976149@qq.com>
2018-01-11 15:28:28 +0800

    rest

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import 'asserts/css/reset.css'
+
+FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router: router,
   template: '<App/>',
   components: { App }
 })
---
+++ src/pages/index/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    123
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index-header'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+</style>
