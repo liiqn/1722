47ad313dc4c127dd67bb9c4a0e0ca38ae8d75b8a
LzDocker@163.com <bjzhangdang@163.com>
2018-01-12 09:53:17 +0800

    add header

---
+++ src/pages/order/index.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+  	<index-header></index-header>
+ 	<index-details></index-details>
+</div>
+</template>
+
+<script>
+  import IndexHeader from './header'
+  import IndexDetails from './details'
+
+  export default {
+    name: 'index',
+    components: {
+      IndexHeader,
+      IndexDetails
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Order from '@/pages/order'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/order',
+      name: 'Order',
+      component: Order
     }
   ]
 })
