f117d4dacdc93feb7d40a0c5dc383c69c0fdc429
LzDocker@163.com <bjzhangdang@163.com>
2018-01-12 10:48:53 +0800

    ddd

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,19 +1,21 @@
 <template>
-   <div>
-  	<index-header></index-header>
- 	<index-details></index-details>
-</div>
+	<div>
+		<index-header></index-header>
+ 	  <index-details></index-details>
+ 	  <index-date></index-date>
+	</div>
 </template>
 
 <script>
   import IndexHeader from './header'
   import IndexDetails from './details'
-
+  import IndexDate from './date'
   export default {
     name: 'index',
     components: {
       IndexHeader,
-      IndexDetails
+      IndexDetails,
+      IndexDate
     }
   }
 </script>
